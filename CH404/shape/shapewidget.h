872160882025e6f6fb0256a14cf560a4660d2de7
Haobo <fundou1081@outlook.com>
2017-10-30 14:31:58 +0800

    added

---
+++ CH404/shape/shapewidget.h
@@ -0,0 +1,27 @@
+﻿#ifndef SHAPEWIDGET_H
+#define SHAPEWIDGET_H
+
+#include <QWidget>
+
+//namespace Ui {
+//class ShapeWidget;
+//}
+
+class ShapeWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ShapeWidget(QWidget *parent = 0);
+    ~ShapeWidget();
+protected:
+    void mousePressEvent(QMouseEvent *);
+    void mouseMoveEvent(QMouseEvent *);
+    void paintEvent(QPaintEvent *);
+
+private:
+//    Ui::ShapeWidget *ui;
+    QPoint dragPosition;
+};
+
+#endif // SHAPEWIDGET_H
